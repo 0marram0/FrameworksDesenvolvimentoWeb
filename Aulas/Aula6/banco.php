c67b47b0cb7b31a65a2e2bfe34dbb13d60e92b9e
Omar <omar.ifpr2024@gmail.com>
2026-06-01 20:58:43 -0300

    Add files via upload

--- Aulas/Aula6/banco.php
+++ Aulas/Aula6/banco.php
@@ -1,1 +1,10 @@
+<?php
+$entidades = [
+    "nome" => ["aluno", "professor", "curso"]
+];
 
+$atributos = [
+    "aluno" => ["nome", "idade", "nascimento"],
+    "professor" => ["nome", "idade", "titulacao"],
+    "curso" => ["nome", "turno", "anos"],
+];
